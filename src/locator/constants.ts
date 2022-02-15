3da9c4412b731ecb27b104c2b9e5d630e6af78e0
Yextds <yext@dotsquares.com>
2022-02-15 17:56:30 +0530

    update files

--- src/locator/constants.ts
+++ src/locator/constants.ts
@@ -6,6 +6,7 @@ export const locationInput = <HTMLInputElement>(
 );
 export const searchButton = document.getElementById("search-location-button");
 // export const useMyLocation = document.getElementById("useLocation");
+export const useMyLocation = '';
 export const locationNoun = "Store";
 export const locationNounPlural = "Stores";
 
